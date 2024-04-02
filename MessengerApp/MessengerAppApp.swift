810ef292f2edea1469b0248303d72734e7f7199b
Juhee <asmilejuliaz@gmail.com>
2024-04-02 15:18:17 +0200

    Initial Commit

---
+++ MessengerApp/MessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerAppApp.swift
+//  MessengerApp
+//
+//  Created by Juhee Kang Johansson on 2024-04-02.
+//
+
+import SwiftUI
+
+@main
+struct MessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
