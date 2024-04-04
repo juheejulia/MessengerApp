fe4abaa99c7f9b5054fc447efa9450aa9f772bd3
Juhee <asmilejuliaz@gmail.com>
2024-04-04 13:52:40 +0200

    Created ChatListRowView File

---
+++ MessengerApp/Views/ChatListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatListRowView.swift
+//  MessengerApp
+//
+//  Created by Juhee Kang Johansson on 2024-04-04.
+//
+
+import SwiftUI
+
+struct ChatListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatListRowView()
+}
