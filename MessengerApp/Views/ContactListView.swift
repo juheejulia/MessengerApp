04ef9663d9355567941ff03a18607f99d927ad84
Juhee <asmilejuliaz@gmail.com>
2024-04-04 10:39:23 +0200

    Add files

---
+++ MessengerApp/Views/ContactListView.swift
@@ -0,0 +1,18 @@
+//
+//  ContactListView.swift
+//  MessengerApp
+//
+//  Created by Juhee Kang Johansson on 2024-04-04.
+//
+
+import SwiftUI
+
+struct ContactListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContactListView()
+}
