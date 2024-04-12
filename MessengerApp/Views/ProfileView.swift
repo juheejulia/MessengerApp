2e7ead20be029dc9f27c9331b3c04bd5014b9442
jammingbadger <christofer2010@live.se>
2024-04-12 11:08:16 +0200

    Profile view

--- MessengerApp/Views/ProfileView.swift
+++ MessengerApp/Views/ProfileView.swift
@@ -8,8 +8,78 @@
 import SwiftUI
 
 struct ProfileView: View {
+   
+    @State var nightMode: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                VStack {
+                
+                    Image("Profile 4")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 120, height: 120)
+                        .mask(Circle())
+                        .padding(20)
+                    Image(systemName: "checkmark.seal.fill")
+                        .font(.system(size: 20))
+                        .foregroundColor(.blue)
+                        .offset(x:50, y: -60)
+                    
+                }
+                
+                
+                VStack {
+                    Text("Profile name")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .offset(x: 1, y: -40)
+                    
+                    
+                }
+                .padding(20)
+                               
+            }
+            
+        }
+        
+        
+        
+        List {
+            Section {
+                Text("Avatar")
+                Text("Notices")
+                Text("Integrity and safety")
+                Toggle("Night Mode", isOn: $nightMode)
+                Text("Password and username")
+            } header: {
+                Text("Settings")
+            }
+            
+        }
+        .scrollContentBackground(.hidden)
+        
+            
+        List {
+            Section {
+                Text("🔥 Fashion")
+                Text("🏈 Sports")
+                Text("🥐 Hidden gems")
+                Text("😂 Funny memes")
+            } header: {
+                Text("joined groups")
+            }
+        }
+        .scrollContentBackground(.hidden)
+        
+        Button {
+            
+        } label: {
+            Text("Sign out")
+        }
+        .tint(.mint)
+        
     }
 }
 
